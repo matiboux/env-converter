558a7fa23783df282a9d9db422b0e5f40086c660
Matiboux <matiboux@gmail.com>
2024-02-24 03:41:12 +0100

    Add i18n strings for footer

--- app/app/src/i18n/locales/en.ts
+++ app/app/src/i18n/locales/en.ts
@@ -2,6 +2,14 @@ const defaultLocale =
 [
 	// Footer
 	'Open source project',
+	'See the source code on',
+	'Built with',
+	'served by',
+	'Made with love by',
+	'Data privacy',
+	'No data is collected or processed over the network or on any server.',
+	'All data is processed locally in your browser, and stays on your own device.',
+	'This website uses no cookies and does no tracking.',
 ] as const
 
 type Keys = typeof defaultLocale[number]
--- app/app/src/i18n/locales/fr.ts
+++ app/app/src/i18n/locales/fr.ts
@@ -3,6 +3,17 @@ import type { I18n, Diff } from '../type'
 const locale =
 {
 	'Open source project': 'Projet open source',
+	'See the source code on': 'Voir le code source sur',
+	'Built with': 'Développé avec',
+	'served by': 'servi par',
+	'Made with love by': 'Créé avec amour par',
+	'Data privacy': 'Confidentialité des données',
+	'No data is collected or processed over the network or on any server.':
+		'Aucune donnée n\'est collectée ou traitée sur le réseau ou sur un serveur.',
+	'All data is processed locally in your browser, and stays on your own device.':
+		'Toutes les données sont traitées localement dans votre navigateur et restent sur votre propre appareil.',
+	'This website uses no cookies and does no tracking.':
+		'Ce site web n\'utilise pas de cookies et ne fait pas de suivi.',
 } as const
 
 // Static type check
