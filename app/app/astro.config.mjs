730ed1d422d67fab9d020ac21a25933419d14c79
Matiboux <matiboux@gmail.com>
2024-02-24 01:58:12 +0100

    Import i18n files & config

--- app/app/astro.config.mjs
+++ app/app/astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config'
 import svelte from '@astrojs/svelte'
 import tailwind from '@astrojs/tailwind'
 
+import i18n from './config/i18n'
+
 // https://astro.build/config
 export default defineConfig({
 	vite: {
@@ -11,6 +13,7 @@ export default defineConfig({
 			},
 		},
 	},
+	i18n: i18n,
 	integrations: [
 		svelte(),
 		tailwind(),
