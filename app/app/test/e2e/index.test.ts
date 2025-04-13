87e259bced639427cd73cd924f94f317d546d86a
Mati <matiboux@gmail.com>
2025-04-13 19:30:58 +0200

    Check test services for non-zero exit codes (#44)

--- app/app/test/e2e/index.test.ts
+++ app/app/test/e2e/index.test.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test'
 test('has title', async ({ page }) =>
 {
 	await page.goto('/')
-	await expect(page).toHaveTitle('Astro Template')
+	await expect(page).toHaveTitle('Env Converter')
 });
