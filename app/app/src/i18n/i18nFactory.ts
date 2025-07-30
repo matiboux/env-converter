c7afa59f877c3e646d625f34c2d70aba839038b4
Matiboux <matiboux@gmail.com>
2025-07-30 03:32:40 +0200

    Update i18n source files

--- app/app/src/i18n/i18nFactory.ts
+++ app/app/src/i18n/i18nFactory.ts
@@ -1,6 +1,6 @@
 import { i18n as i18nConfig } from '~/config'
 import { i18n } from './i18n'
-import type { Tail } from './type'
+import type { Tail } from './types.d.ts'
 
 export function i18nFactory(locale: Parameters<typeof i18n>[0] | undefined)
 {
